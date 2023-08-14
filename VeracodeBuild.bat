0997dc6ab741ce1442bf2e1a5f75d84bc5e34869
eddemo0007 <108079639+eddemo0007@users.noreply.github.com>
2023-08-14 12:01:12 -0400

    Update VeracodeBuild.bat

--- VeracodeBuild.bat
+++ VeracodeBuild.bat
@@ -28,8 +28,8 @@ SET VERACODE_JAR_PATH=.\VeracodeJavaAPI.jar
 SET API_ID=8e3c8af9f16be054867c9344ce74090e
 SET API_KEY=3e95620ba25005d71b4d983a178454d9efe0836e6c00e60def419649472f3023878512af1d2aa24ec3950afc92aac916013add32409b9e4d60f623eb978546ff
 SET APP_NAME=GoCDVerademo-dotnet0001
-SET SCAN_NAME=Scan008
-SET VERSION=Version008
+SET SCAN_NAME=Scan009
+SET VERSION=Version009
 SET SCAN_PDF_REPORT=scan_report.pdf
 SET FILEPATH=C:\local-repos\verademo-dotnet\app\bin.zip
 
@@ -47,7 +47,7 @@ ECHO End of: Check Scan Status 001
 
 ECHO Start of: Check Scan Status 002
 REM Check the scan status
-IF "%SCAN_STATUS%" EQU "ResultsReady" (
+IF "%SCAN_STATUS%" == "ResultsReady" (
     REM Download PDF report
     java -jar "%VERACODE_JAR_PATH%" -vid "%API_ID%" -vkey "%API_KEY%" -action summaryreport -appname "%APP_NAME%" -buildname "%SCAN_NAME%" -outputfile "%SCAN_PDF_REPORT%"
 ) ELSE (
